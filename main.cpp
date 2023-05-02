d387e1e61e5c2453b9c7e3fece6fdd66d633c5b6
willemromao <franciscowillem@gmail.com>
2023-05-01 23:37:53 -0300

    _

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+/**
+ * @file main.cpp
+ * @brief Programa principal do sistema.
+ */
+
 #include <iostream>
 #include "LinkedList.h"
 #include "Menu.h"
 
+/**
+ * @brief Função principal do programa.
+ * @details Cria uma lista encadeada e um objeto do menu, exibindo-o em seguida.
+ * @return Retorna 0 se a execução foi bem sucedida.
+ */
 int main() {
   
   LinkedList<int> *lista = new LinkedList<int>();
