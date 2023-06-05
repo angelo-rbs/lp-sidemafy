6e916f4303a7f51e36d1b450cf39dc2d117e5c2e
angelo-rbs <helenafranca1973@gmail.com>
2023-06-05 12:27:17 -0300

    PlaylistManager print() e create()

--- include/PlaylistsManager.h
+++ include/PlaylistsManager.h
@@ -31,6 +31,47 @@ class PlaylistsManager {
 
     // funcionalidades
 
+    void create(Playlist &pl) {
+      playlists->append(pl);
+    }
+
+    bool remove(Playlist &pl) {
+      int pos = playlists->find(pl);
+
+      if (pos != NOT_FOUND) return playlists->remove(pos);
+      else return false;
+    }
+
+    bool update(Playlist &pl) {
+      int pos = playlists->find(pl);
+
+      if (pos != NOT_FOUND) return (playlists->insert(pos, pl) != nullptr);
+    }
+
+    void print() {
+      int pos = 1;
+      Node<Playlist>* node = playlists->getHead();
+
+      std::cout << "================================================================================" << std::endl;
+      std::cout << "Lista de playlists cadastradas" << std::endl;
+      std::cout << "================================================================================" << std::endl;
+
+      if (node != nullptr) {
+        while (node != nullptr) {
+
+          Playlist pl = node->getValue();
+
+          std::cout << pos << " - " << pl.getName() << ", " << pl.getMusics()->getSize() << " músicas" <<std::endl;
+
+          ++pos;
+          node = node->getNext();
+        }
+      } else
+        std::cout << "[lista vazia]" << std::endl;
+
+      std::cout << "================================================================================" << std::endl;
+    }
+
     void purge(Music *song) {
 
       Node<Playlist> *it = playlists->getHead();
