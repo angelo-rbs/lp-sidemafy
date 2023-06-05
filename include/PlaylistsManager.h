d48061774e8aeab18107807330fc47d5023419f0
angelo-rbs <helenafranca1973@gmail.com>
2023-06-05 01:02:08 -0300

    PlaylistsManager base e purge()

--- include/PlaylistsManager.h
+++ include/PlaylistsManager.h
@@ -9,8 +9,43 @@ class PlaylistsManager {
 
   private:
 
+    LinkedList<Playlist> *playlists;
+
   public:
 
+    // construtores
+
+    PlaylistsManager() {
+      this->playlists = new LinkedList<Playlist>();
+    }
+
+    PlaylistsManager(LinkedList<Playlist> &playlist) {
+      this->playlists = new LinkedList(playlist);
+    }
+
+    // getter  
+
+    LinkedList<Playlist>* getPlaylists() {
+      return playlists;
+    }
+
+    // funcionalidades
+
+    void purge(Music *song) {
+
+      Node<Playlist> *it = playlists->getHead();
+
+      while (it != nullptr) {
+
+        Playlist pl = it->getValue();
+        
+        if (pl.find(song) != -1)
+          pl.remove(song);
+
+        it = it->getNext();
+      }
+
+    }
 };
 
 #endif
